88570b48789caf1d750ad4a42db8988e0bc38c48
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-10-01 14:23:04 +0700

    Using serve
    Can use another alternative

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
+echo 'Installing serve just for testing our app can be delivered correctly'
 set -x
-npm run build
+npm install -g serve
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
+echo 'Run the build result using serve and test your react app use this'
 set -x
-npm start &
+serve -s build &
 sleep 1
 echo $! > .pidfile
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
