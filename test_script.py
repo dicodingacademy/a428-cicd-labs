e52e689c50e8f9408ef1964974f4f0c7e8a60795
Syahrul Rizki <62525690+syahrulrzk@users.noreply.github.com>
2023-11-24 09:22:49 +0700

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,11 @@
+# your_test_script.py
+
+import unittest
+
+class TestYourApp(unittest.TestCase):
+
+    def test_example(self):
+        self.assertTrue(True, "Example test passed!")
+
+if __name__ == "__main__":
+    unittest.main()
