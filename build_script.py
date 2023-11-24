f474a96aa30e392a119858c52757c40512bfdcb9
Syahrul Rizki <62525690+syahrulrzk@users.noreply.github.com>
2023-11-24 22:58:44 +0700

    Update build_script.py

--- build_script.py
+++ build_script.py
@@ -2,12 +2,14 @@ import subprocess
 import sys
 
 def install_dependencies():
-    subprocess.run([sys.executable, "-m", "venv", "venv"])
-    subprocess.run(["venv/bin/python", "-m", "pip", "install", "-r", "requirements.txt"])
-    if result.returncode != 0:
-        print("Error installing dependencies:")
-        print(result.stderr.decode("utf-8"))
-        exit(1)
+    try:
+        print("Installing dependencies...")
+        subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
+        subprocess.run(["venv/bin/python", "-m", "pip", "install", "-r", "requirements.txt"], check=True)
+        print("Dependencies installed successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 def main():
     print("Building the Python project...")
